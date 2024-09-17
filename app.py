eea6914d828fc0338f1993e8c4028dedfc6fb0af
Rosalie <144969642+rhughes65@users.noreply.github.com>
2024-09-17 11:12:07 -0600

    changed python main file to python and flask main file

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, send_file
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+        <form action="/scrape" method="post">
+            <label for="url">Enter URL:</label>
+            <input type="text" id="url" name="url">
+            <input type="submit" value="Scrape">
+        </form>
+    '''
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    url = request.form['url']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extract data (example: all links)
+    data = [link.get('href') for link in soup.find_all('a')]
+
+    # Write data to Excel
+    workbook = Workbook()
+    sheet = workbook.active
+    for row, item in enumerate(data, start=1):
+        sheet.cell(row=row, column=1, value=item)
+    filename = 'output.xlsx'
+    workbook.save(filename)
+
+    return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
